90363e44e21b1e5c2b2a3c70381e6c55fda9ed4e
Dvimin <dmitriev.michail@mail.ru>
2025-02-09 21:21:02 +0300

    Added WM_SIZE handling and swap chain resizing

--- 1lab/src/main.cpp
+++ 1lab/src/main.cpp
@@ -1,45 +1,114 @@
-﻿#define UNICODE  // Используем Unicode-версию WinAPI
+﻿#define UNICODE
 #include <windows.h>
+#include <d3d11.h>
+#include <dxgi.h>
 
-// Обработчик сообщений окна
+#pragma comment(lib, "d3d11.lib")
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+// Глобальные переменные DirectX
+ID3D11Device* device = nullptr;
+ID3D11DeviceContext* context = nullptr;
+IDXGISwapChain* swapChain = nullptr;
+ID3D11RenderTargetView* renderTargetView = nullptr;
+
+// Функция изменения размера окна
+void ResizeD3D(UINT width, UINT height) {
+    if (swapChain) {
+        // Освобождаем старый рендер-таргет
+        if (renderTargetView) renderTargetView->Release();
+
+        // Изменяем размер буфера swap chain
+        swapChain->ResizeBuffers(0, width, height, DXGI_FORMAT_UNKNOWN, 0);
+
+        // Получаем новый back buffer
+        ID3D11Texture2D* backBuffer = nullptr;
+        swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backBuffer);
+        device->CreateRenderTargetView(backBuffer, nullptr, &renderTargetView);
+        backBuffer->Release();
+
+        // Устанавливаем новый рендер-таргет
+        context->OMSetRenderTargets(1, &renderTargetView, nullptr);
+    }
+}
+
+void InitD3D(HWND hWnd) {
+    DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
+    swapChainDesc.BufferCount = 1;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.OutputWindow = hWnd;
+    swapChainDesc.SampleDesc.Count = 1;
+    swapChainDesc.Windowed = TRUE;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+    D3D_FEATURE_LEVEL featureLevel;
+    D3D11CreateDeviceAndSwapChain(
+        nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0,
+        D3D11_SDK_VERSION, &swapChainDesc, &swapChain,
+        &device, &featureLevel, &context
+    );
+
+    ID3D11Texture2D* backBuffer = nullptr;
+    swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backBuffer);
+    device->CreateRenderTargetView(backBuffer, nullptr, &renderTargetView);
+    backBuffer->Release();
+
+    // Устанавливаем рендер-таргет
+    context->OMSetRenderTargets(1, &renderTargetView, nullptr);
+}
+
+void RenderFrame() {
+    FLOAT clearColor[4] = { 0.0f, 0.2f, 0.4f, 1.0f }; // Синий фон
+    context->ClearRenderTargetView(renderTargetView, clearColor);
+    swapChain->Present(1, 0);
+}
+
+void CleanupD3D() {
+    if (renderTargetView) renderTargetView->Release();
+    if (swapChain) swapChain->Release();
+    if (context) context->Release();
+    if (device) device->Release();
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"DirectXWindowClass";
+    RegisterClass(&wc);
+
+    HWND hWnd = CreateWindowEx(0, wc.lpszClassName, L"DirectX 11 Window",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        nullptr, nullptr, hInstance, nullptr);
+
+    ShowWindow(hWnd, nCmdShow);
+    InitD3D(hWnd);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            RenderFrame(); // Рисуем кадр
+        }
+    }
+
+    CleanupD3D();
+    return 0;
+}
+
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
+    case WM_SIZE:
+        ResizeD3D(LOWORD(lParam), HIWORD(lParam)); // Меняем размер swap chain
+        return 0;
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;
     }
     return DefWindowProc(hWnd, message, wParam, lParam);
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    // Регистрация класса окна
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"DirectXWindowClass";  // Unicode строка
-
-    RegisterClass(&wc);
-
-    // Создание окна (теперь используется CreateWindowExW)
-    HWND hWnd = CreateWindowEx(
-        0,
-        wc.lpszClassName,
-        L"DirectX 11 Window",  // Unicode строка
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        nullptr, nullptr, hInstance, nullptr
-    );
-
-    if (!hWnd) return -1;
-
-    ShowWindow(hWnd, nCmdShow);
-
-    // Цикл обработки сообщений
-    MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
 }
